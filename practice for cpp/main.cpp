3496e4e5a0bd4494ea2b68d84e56ac84a4d547e1
maejo-o <efforterjisulee@gmail.com>
2023-03-09 01:05:36 +0900

    Initial Commit

---
+++ practice for cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  practice for cpp
+//
+//  Created by 이지수 on 2023/03/09.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
