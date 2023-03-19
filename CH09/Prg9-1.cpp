30b7ff68a014c73926a01cc072ef7c512fac6b85
maejo-o <efforterjisulee@gmail.com>
2023-03-20 00:04:12 +0900

    CH09 start

---
+++ CH09/Prg9-1.cpp
@@ -0,0 +1,16 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int score = 92;
+    int& rScore = score;
+    cout << "acessing with data var" << endl;
+    cout << "score : " << score << endl;
+    
+    cout << "acessing with reference var" << endl;
+    cout << "score : " << rScore << endl;
+    return 0;
+}
+*/
