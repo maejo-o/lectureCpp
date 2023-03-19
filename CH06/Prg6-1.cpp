f0e1fb01c14c4c9b8b869e7f9e4cefec7e4b1916
maejo-o <efforterjisulee@gmail.com>
2023-03-19 12:35:49 +0900

    CH06

---
+++ CH06/Prg6-1.cpp
@@ -0,0 +1,30 @@
+/*
+# include <iostream>
+# include <cmath>
+using namespace std;
+
+int main(void)
+{
+    // using abs Func
+    cout << "abs(8) : " << abs(8) << endl;
+    cout << "abs(-8 : " << abs(-8) << endl;
+    
+    // using floor, ceil Func
+    cout << "floor(12.78) : " << floor(12.78) << endl;
+    cout << "ceil(12.78) : " << ceil(12.78) << endl;
+    
+    // using log, log10 Func
+    cout << "log(100) : " << log(100) << endl;
+    cout << "log10(100) : " << log10(100) << endl;
+    
+    // using exp, pow Func
+    cout << "exp(5) : " << exp(5) << endl;
+    cout << "pow(2,3) : " << pow(2,3) << endl;
+    
+    // using sqrt Func
+    cout << "sqrt(100) : " << sqrt(100);
+    
+    cout << "\n";
+    return 0;
+ }
+*/
