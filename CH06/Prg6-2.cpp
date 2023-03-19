1aa0a9537ea9ac3ced8c8ca889ef36547c8000ac
maejo-o <efforterjisulee@gmail.com>
2023-03-19 12:43:31 +0900

    Prg6-2

---
+++ CH06/Prg6-2.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(void)
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    
+    double term;
+    
+    // get a, b, c
+    cout << " a : ";
+    cin >> a;
+    cout << " b : ";
+    cin >> b;
+    cout << " c : ";
+    cin >> c;
+    
+    // get term
+    term = pow(b,2) - 4*a*c;
+    
+    // discriminant
+    if(term==0)
+    {
+        cout << "one" << endl;
+    }
+    else if (term > 0)
+    {
+        cout << "two" << endl;
+    }
+    else
+    {
+        cout << "None" << endl;
+    }
+    
+    return 0;
+}
