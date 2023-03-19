414e5f5e1342c44a9d78e8a397c347c3cfa9091e
maejo-o <efforterjisulee@gmail.com>
2023-03-19 19:00:16 +0900

    Prg6-19.cpp

---
+++ CH06/Prg6-18.cpp
@@ -0,0 +1,19 @@
+/*
+#include <iostream>
+using namespace std;
+
+double calcEarnings(double rate, double hours =40);
+
+int main()
+{
+    cout << "employee1 fea : " << calcEarnings(22.0) << endl;
+    cout << "employee2 fea : " << calcEarnings(12.50, 18) << endl;
+    return 0;
+}
+double calcEarnings(double rate, double hours)
+{
+    double pay;
+    pay = hours * rate;
+    return pay;
+}
+*/
---
+++ CH06/Prg6-19.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+using namespace std;
+
+int max(int num1, int num2);
+int max(int num1, int num2, int num3);
+int max(int num1, int num2, int num3, int num4);
+
+int main(void)
+{
+    cout << "maximum(5,7) = " << max(5, 7) << endl;
+    cout << "maximum(7, 9, 8) = " << max(7, 9, 8) << endl;
+    cout << "maximum(14, 3, 12, 11)  = " << max(14, 3, 12, 11) << endl;
+    return 0;
+}
+int max(int num1, int num2)
+{
+    int larger;
+    if(num1>=num2)
+    {
+        larger = num1;
+    }
+    else
+        larger = num2;
+    return larger;
+}
+int max(int num1, int num2, int num3)
+{
+    return max(max(num1, num2), num3);
+}
+int max(int num1, int num2, int num3, int num4)
+{
+    return max(max(num1, num2, num3), num4);
+}
+
