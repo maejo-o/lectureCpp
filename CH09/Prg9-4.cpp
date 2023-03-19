21f970c8a8a2fe608bf18f409271f8279f5d7bdc
maejo-o <efforterjisulee@gmail.com>
2023-03-20 01:45:19 +0900

    last CH09 commit

---
+++ CH09/Prg9-4.cpp
@@ -0,0 +1,28 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool flag = true;
+    int score = 92;
+    double average = 82.56;
+    
+    cout << "var of bool" << endl;
+    cout << "size : " << sizeof(flag) << "\t";
+    cout << "value : " << flag << "\t";
+    cout << "adress : " << &flag << endl << endl;
+    
+    cout << "var of int" << endl;
+    cout << "size : " << sizeof(score) << "\t";
+    cout << "value : " << score << "\t";
+    cout << "adress : " << &score << endl << endl;
+    
+    cout << "var of double" << endl;
+    cout << "size : " << sizeof(average) << "\t";
+    cout << "value : " << average << "\t";
+    cout << "adress : " << &average << endl << endl;
+    
+    return 0;
+}
+*/
---
+++ CH09/Prg9-5.cpp
@@ -0,0 +1,14 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int score = 92;
+    int* pScore = &score;
+    
+    cout << "acessing the score directly: " << score << endl;
+    cout << "acessing the score indirectly : " << *pScore << endl;
+    return 0;
+}
+*/
