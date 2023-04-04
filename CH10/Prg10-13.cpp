a3fccc7cc4d7daf7191379a03ea5d1770270b9f4
maejo-o <efforterjisulee@gmail.com>
2023-04-04 22:23:11 +0900

    input the string

---
+++ CH10/Prg10-13.cpp
@@ -0,0 +1,13 @@
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string strg;
+    cout << "Input the String : ";
+    cin >> strg;
+    cout << strg << endl;
+    return 0;
+}
