0ab6819e96aef188a2d1a12a4ab3dde22318428a
maejo-o <efforterjisulee@gmail.com>
2023-04-08 16:00:02 +0900

    CH07 start

---
+++ CH07/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CH07
+//
+//  Created by 이지수 on 2023/04/08.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
