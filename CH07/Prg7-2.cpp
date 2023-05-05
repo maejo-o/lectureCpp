f0a3f8e81f4f19266772ab0a33759a471c7536eb
maejo-o <efforterjisulee@gmail.com>
2023-05-06 01:15:41 +0900

    circle class

--- CH07/Prg7-2.cpp
+++ CH07/Prg7-2.cpp
@@ -1,78 +1,78 @@
-/*
+// 객체 지향 프로그램에서 클래스를 사용하는 예
 #include <iostream>
 using namespace std;
-
+// 1. class definition
 class Circle
 {
 private:
     double radius;
 public:
-    Circle(double radius);
     Circle();
+    Circle(double radius);
+    Circle(const Circle& cn);
     ~Circle();
-    Circle(const Circle& circle);
-    // void setRadius(double value);
+    void setRadius(double value);
     double getRadius() const;
     double getArea() const;
     double getPerimeter() const;
 };
-
+// 2. member function definition
+// constructor without parameter, constructor with parameter, copy constructor, destructor, member function definition
+Circle::Circle()
+{
+    cout << "constructor without parameter" << endl;
+}
 Circle::Circle(double rds)
 :radius(rds)
 {
-    cout << "매개변수가 있는 생성자가 호출 되었습니다." << endl;
+    cout << "constructor with parameter" << endl;
 }
-
-Circle::Circle()
-:radius(0.0)
+Circle::Circle(const Circle& cn)
+:radius(cn.radius)
 {
-    cout << "기본생성자가 호출 되었습니다." << endl;
-}
-Circle::Circle(const Circle& circle)
-:radius(circle.radius)
-{
-    cout << "복사 생성자가 호출 되었습니다." << endl;
+    cout << "copy constructor" << endl;
 }
 Circle::~Circle()
 {
-    cout << "소멸자가 호출 되었습니다. " << radius << endl;
+    cout << "deconstructor" << endl;
 }
-// void Circle::setRadius(double value)
-// {
-//    value = radius;
-// }
+void Circle::setRadius(double value)
+{
+    radius = value;
+}
 double Circle::getRadius() const
 {
     return radius;
 }
 double Circle::getArea() const
 {
-    const double PI = 3.14;
-    return(PI*radius*radius);
+    const int PI = 3.14;
+    return PI*radius*radius;
 }
 double Circle::getPerimeter() const
 {
-    const double PI = 3.14;
-    return(2*PI*radius);
+    const int PI = 3.14;
+    return PI*2*radius;
 }
-
-int main(void)
+// 3. application
+int main()
 {
     Circle circle1(5.2);
+    cout << "<circle1>" << endl;
     cout << "radius: " << circle1.getRadius() << endl;
     cout << "area: " << circle1.getArea() << endl;
     cout << "perimeter: " << circle1.getPerimeter() << endl << endl;
-    
     Circle circle2(circle1);
+    cout << "<circle2>" << endl;
     cout << "radius: " << circle2.getRadius() << endl;
     cout << "area: " << circle2.getArea() << endl;
-    cout << "perimeter: " << circle2.getPerimeter() << endl << endl;
+    cout << "perimeterL " << circle2.getPerimeter() << endl << endl;
     
     Circle circle3;
-    cout << "radiius: " << circle3.getRadius() << endl;
+    cout << "<circle3>" << endl;
+    cout << "radius: " << circle3.getRadius() << endl;
     cout << "area: " << circle3.getArea() << endl;
-    cout << "perimter: " << circle3.getPerimeter() << endl << endl;
+    cout << "perimeter: " << circle3.getPerimeter() << endl << endl;
+    
     return 0;
 }
-
-*/
